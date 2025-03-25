1fab532ab604bb71259d21fb3a1dd800e2d99266
Justmoong <andudyun0504@gmail.com>
2025-03-25 12:15:01 +0900

    Fixed at: 2025-03-25 12:15:01

--- Setup.sh
+++ Setup.sh
@@ -6,6 +6,14 @@ sudo -v
 
 chmod +x /Users/ymy/.dotfiles/Scripts/sudo.sh
 chmod +x /Users/ymy/.dotfiles/Scripts/symlink.sh
+chmod +x /Users/ymy/.dotfiles/Scripts/InstallHomebrew.sh
+chmod +x /Users/ymy/.dotfiles/Scripts/launchctl.sh
 
 /Users/ymy/.dotfiles/Scripts/sudo.sh
 /Users/ymy/.dotfiles/Scripts/symlink.sh
+/Users/ymy/.dotfiles/Scripts/InstallHomebrew.sh
+/Users/ymy/.dotfiles/Scripts/launchctl.sh
+
+wait 10
+
+#/Users/ymy/.dotfiles/.macos
