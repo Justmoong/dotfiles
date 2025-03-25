463694c3dd8b174f49cb31b39af4b85635f97a65
Justmoong <andudyun0504@gmail.com>
2025-03-25 12:25:04 +0900

    Fixed at: 2025-03-25 12:25:04

--- Scripts/launchctl.sh
+++ Scripts/launchctl.sh
@@ -1,7 +1,7 @@
 # launchctl load ~/Library/LaunchAgents/com.user..plist
 # launchctl start com.user.
 
-cp ~/.dotfiles/Scripts/launchd/*.plist ~/Library/LaunchAgents
+cp -fv ~/.dotfiles/Scripts/launchd/*.plist ~/Library/LaunchAgents
 
 launchctl load ~/Library/LaunchAgents/com.user.SafariAutomaticStableDiffusionWindow.plist
 launchctl load ~/Library/LaunchAgents/com.user.LaunchTasks.plist
