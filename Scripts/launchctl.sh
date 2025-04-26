6bb962aa73da00764e2e1c7a073d6980492b356a
Justmoong <andudyun0504@gmail.com>
2025-04-26 23:05:54 +0900

    Fixed at: 2025-04-26 23:05:54

--- Scripts/launchctl.sh
+++ Scripts/launchctl.sh
@@ -5,8 +5,10 @@ cp -fv ~/.dotfiles/Scripts/launchd/*.plist ~/Library/LaunchAgents
 
 launchctl load ~/Library/LaunchAgents/com.user.SafariAutomaticStableDiffusionWindow.plist
 launchctl load ~/Library/LaunchAgents/com.user.LaunchTasks.plist
+launchctl load ~/Library/LaunchAgents/com.user.LaunchMusicPlay.plist
 
 launchctl start com.user.SafariAutomaticStableDiffusionWindow
 launchctl start com.user.LaunchTasks
+launchctl start com.user.LaunchMusicPlay
 
 ls -l ~/Library/LaunchAgents
