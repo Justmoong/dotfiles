9abeefd60823419bf3efd439d5c166250808d386
root <root@Debian.iisacc.main>
2025-05-13 12:02:15 +0900

    Fixed at: 2025-05-13 12:02:15

--- shell/symlink.sh
+++ shell/symlink.sh
@@ -1,13 +1,13 @@
 #!/bin/zsh
 
 ln -sf ~/.dotfiles/.config ~/.config
-ln -sf ~/.dotfiles/macOS/shell/.oh-my-zsh ~/.oh-my-zsh
+ln -sf ~/.dotfilesshell/.oh-my-zsh ~/.oh-my-zsh
 
-ln -sf ~/.dotfiles/macOS/shell/.zshrc ~/.zshrc
-ln -sf ~/.dotfiles/macOS/shell/.zshenv ~/.zshenv
-ln -sf ~/.dotfiles/macOS/shell/.zprofile ~/.zprofile
-ln -sf ~/.dotfiles/macOS/shell/.zlogin ~/.zlogin
-ln -sf ~/.dotfiles/macOS/shell/.profile ~/.profile
+ln -sf ~/.dotfiles/shell/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/shell/.zshenv ~/.zshenv
+ln -sf ~/.dotfiles/shell/.zprofile ~/.zprofile
+ln -sf ~/.dotfiles/shell/.zlogin ~/.zlogin
+ln -sf ~/.dotfiles/shell/.profile ~/.profile
 
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/.gitignore ~/.gitignore
